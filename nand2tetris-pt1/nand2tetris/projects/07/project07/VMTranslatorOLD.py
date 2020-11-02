ae70c76e82c4ebdcf3fba1babf45fbfa31c3c39b
Adel elmala <adel.elmala2025@gmail.com>
2020-11-02 14:44:04 +0200

    Solved Project 7

---
+++ nand2tetris-pt1/nand2tetris/projects/07/project07/VMTranslatorOLD.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python
+
+from parser import Tokenizer
+from codeWriter import CodeWriter
+import re
+import os
+import glob
+import sys
+
+class VMtranslator():
+    """
+        Translate VM command to Hack-assembly commands 
+
+    """
+    def __init__(self,path):
+        self.path = path
+        self.fileName = ''
+        self.folderName = ''
+        self.writeToFile = ''
+        self.writeToFolder = ''
+        self.fileNoExt = ''
+        # self.parser = Tokenizer()
+
+        # self.VMtranslator()
+        self.identifyPath()
+
+    def identifyPath(self):
+        # path = self.path.split('/')
+        path = re.split(r'[ / \ ]',self.path)
+        last = path[-1]
+        
+        
+        # print(self.writeToFolder)
+        pattern = re.compile(r'.*[.]vm$')
+        match = re.match(pattern,last)
+        if match: # vm File
+            print("in file")
+            self.fileNoExt = last.partition('.')[0]
+            self.writeToFile = self.fileNoExt + '.asm'
+
+            # self.writeToFolder = '/'.join(path[:-1])
+            self.fileName = last
+            self.translateFile(self.fileName)
+        else : # Folder
+            print("in folder")
+            self.writeToFolder = self.path
+            self.writeToFile =  self.writeToFolder + self.fileName.partition('.')[0] + ".asm"
+            self.folderName = last
+            # print(self.writeToFolder)
+            self.translateFolder(self.writeToFolder)
+
+    
+    
+    def translateFile(self,fileName):
+        # print(fileName)
+
+        parser = Tokenizer(fileName)
+        commands = parser.tokenizer()   # List of vm commands
+        # print(commands)
+        coder = CodeWriter()
+        with open( self.writeToFile, 'w') as writer:
+            # print(file)
+            for i,cmd in enumerate(commands) :
+                # print(i,cmd)
+                translated = coder.writeCode(cmd,i,self.fileNoExt) 
+                # print(translated)
+                translated = [f"//{cmd}"] + translated
+                #  translated
+                [writer.write(tcmd + '\n') for tcmd in translated]
+
+    def translateFolder(self,folder):
+        # for filename in os.listdir(f'{folder}'):
+        # path = '/some/path/to/file'
+        for filename in glob.glob(os.path.join(folder, '*.vm')):
+            print("+++++")
+            print(filename)
+            self.fileName = filename
+            self.writeToFile =  self.fileName.partition('.')[0] + ".asm"
+
+            # print(self.writeToFolder + '/'+ self.fileName)
+
+            self.translateFile(self.fileName)
+
+
+
+# if name == "__main"    
+
+# translate = VMtranslator("BasicTest.vm")
+# translate = VMtranslator("tet")
+# translate.identifyPath()
+# translate.translateFolder("BasicTest.vm")
+
+def main():
+    path = sys.argv[-1]    
+    VMtranslator(path)
+    
+if __name__ == '__main__':
+    main()
---
+++ nand2tetris-pt1/nand2tetris/projects/07/project07/parser.py
@@ -0,0 +1,55 @@
+import re
+
+class Tokenizer():
+    
+    def __init__(self,fileName):
+        self.fileName = fileName
+
+        
+    
+    def readFile(self):
+        # Check if its right extention
+        pattern = re.compile(r'.*[.]vm')
+
+        match = re.match(pattern,self.fileName)
+        # print(self.fileName)
+
+        if match:
+            with open(self.fileName, 'r') as reader:
+                lines = reader.readlines()
+                
+                # print(lines)
+                return lines
+        else:
+            raise "File Extenstion not supported"
+   
+
+
+    def filterLine(self,line):
+        lineList = line.split()
+        # print(lineList)
+        return ((lineList == []) or (lineList[0] == '//'))
+
+    def tokens(self,line):
+        triple = line.partition('//') # Remove in-line comments
+        token  = triple[0].rstrip('\n') # Remove '\n' chars
+        token  = token.strip()  # Remove leading and trailing whitespaces
+        # print(triple)
+        return token
+
+    def filter(self,list):
+        filterList = [ self.tokens(line) for line in list if not(self.filterLine(line)) ]    
+
+        # print(filterList)
+        
+        return filterList
+
+    def tokenizer(self):
+        file = self.readFile()
+        return self.filter(file)    
+
+
+
+# token = Tokenizer("~/papl/Nand2Tetris/nand2tetris-pt1/nand2tetris/projects/07/StackArithmetic/SimpleAdd/SimpleAdd.vm")
+# token = Tokenizer("StackTest.vm")
+# token.tokenizer()
